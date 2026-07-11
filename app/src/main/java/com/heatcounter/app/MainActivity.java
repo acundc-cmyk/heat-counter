14febf08793fd421840dab2a5d53cb356fd99f51
acundc-cmyk <acundc@gmail.com>
2026-07-12 01:50:51 +0200

    Add files via upload

---
+++ app/src/main/java/com/heatcounter/app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.heatcounter.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        web = new WebView(this);
+        WebSettings s = web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);   // localStorage bleibt gespeichert
+        s.setDatabaseEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+
+        setContentView(web);
+        web.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && web != null && web.canGoBack()) {
+            web.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
